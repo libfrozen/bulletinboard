2722c0576d8d1ffd8bc8456b139947d90bb65a61
libfridge <libfrozen.so@outlook.com>
2019-05-24 23:36:44 +0800

    没什么改动 改了改可读性

--- bulletinboard.lua
+++ bulletinboard.lua
@@ -9,7 +9,7 @@ for address,d_type in cpm.list("modem") do
   local modem = cpm.proxy(address)
   local isornot = modem.isWireless()
   if isornot == true then
-   table.insert(wirelessModem,address)
+    table.insert(wirelessModem,address)
   end
   local modem = nil
 end
@@ -17,7 +17,7 @@ end
 function table_count(table_name)
   local leng = 0
   for k,v in pairs(table_name) do
-   leng = leng + 1
+     leng = leng + 1
   end
   return leng
 end
@@ -28,13 +28,14 @@ if table_count(wirelessModem) == 0 then
   os.exit()
 end
 
-
-if table_count(wirelessModem) == 1 then
+if component.isAvailable("modem") then
+ if table_count(wirelessModem) == 1 then
   local i = 1
   modem = cpm.proxy(wirelessModem[i])
   csn = wirelessModem[i]
   print("Now using card " .. wirelessModem[i])
-else if table_count(wirelessModem) > 1 then
+  end
+ else if table_count(wirelessModem) > 1 then
   print("You have installed more than 1 wireless modems.Please choose one.")
   for k,v in pairs(wirelessModem) do
      print(k .. " " .. v)
@@ -42,14 +43,16 @@ else if table_count(wirelessModem) > 1 then
   print("Type a number.")
   local i = tonumber(io.read())
   if i < 1 or i > table_count(wirelessModem) or i == nil or not type(i) == "number" then
-   print("Wrong input.")
-   os.exit()
+    print("Wrong input.")
+    os.exit()
   else
-   modem = cpm.proxy(wirelessModem[i])
-   csn = wirelessModem[i]
-   print("Now using card " .. wirelessModem[i])
+    modem = cpm.proxy(wirelessModem[i])
+    csn = wirelessModem[i]
+    print("Now using card " .. wirelessModem[i])
   end
   end
+else
+  print
 end
 
 --Port selection.
@@ -85,19 +88,19 @@ function receiver(event_name,localNetworkCard,remoteAddress,port,distance,payloa
   print("[" .. timer() .. "]" .. "Received data '" .. tostring(payload) .. "' from address " .. remoteAddress ..
         " on network card " .. localNetworkCard .. " on port " .. port .. ".")
   if distance > 0 then
-  print("[" .. timer() .. "]" .. "Message was sent from " .. distance .. " blocks away.")
+    print("[" .. timer() .. "]" .. "Message was sent from " .. distance .. " blocks away.")
   end
 end
 
 function simplify(string)
-   local simplified = string.sub(string,1,6)
-   return simplified
+  local simplified = string.sub(string,1,6)
+  return simplified
 end
 
 function receiver2(event_name,localNetworkCard,remoteAddress,port,distance,payload)
-print("[" .. simplify(remoteAddress) .. "..." .. "]" .. tostring(payload))
+  print("[" .. simplify(remoteAddress) .. "..." .. "]" .. tostring(payload))
 end
 
 while true do
-receiver2(event.pull("modem_message"))
+  receiver2(event.pull("modem_message"))
 end
